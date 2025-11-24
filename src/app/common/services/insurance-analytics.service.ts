6478ba26ad21212e1bf213d39e0e294c7439ca37
Jeet Dank <gdank5898@gmail.com>
2025-11-24 15:57:42 +0530

    basic apis added

--- src/app/common/services/insurance-analytics.service.ts
+++ src/app/common/services/insurance-analytics.service.ts
@@ -1,9 +1,63 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_ENDPOINTS as apis } from '../constants/API_ENDPOINTS';
+
+
+interface multipleCompanies { 
+   "company_inputs": string[]
+}
+
+interface ambiguityResolve{
+   metric_name: string;
+    context: string;
+    suggestions: string[];
+    resolution_type: string;
+}
+
+interface batchAnalysis{
+   "companies": companies[],
+    "time_periods": string[],
+    "filing_type": string,
+    "analysis_depth": number
+}
+
+interface companies{
+  cik:string;
+  name:string;
+}
+
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InsuranceAnalyticsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  parseQuery(payload: { query: string }): Observable<any> {
+    return this.http.post(apis.PARSE_QUERY, payload);
+  }
+
+
+  resolveMultipleCompanies(payload:multipleCompanies):Observable<any>{
+    return this.http.post(apis.COMPANY_RESOLVE_MULTIPLE,payload);
+  }
+
+
+  resolveAmbiguities(payload:ambiguityResolve):Observable<any>{
+    return this.http.post(apis.AMBIGUITY_RESOLVE,payload);
+  }
+
+
+  batchAnalysis(payload:batchAnalysis){
+    return this.http.post(apis.BATCH_ANALYSIS,payload);
+  }
+
+
+
+
+
+
+
 }
